839a3bf868c562c98a16603d6552b65591cab570
Marek Sontag <mareksontag@gmail.com>
2020-02-19 15:46:11 +0100

    Read sex from pesel

--- src/main/java/pl/sda/javabasics/zus/bok/PeselUtils.java
+++ src/main/java/pl/sda/javabasics/zus/bok/PeselUtils.java
@@ -8,4 +8,13 @@ class PeselUtils {
         return pesel != null && pesel.matches("[0-9]{11}");
     }
 
+    static char readSex(String pesel) {
+        if (validate(pesel)) {
+            String sexDigitAsString = pesel.substring(9, 10);
+            int sexAsNumber = Integer.parseInt(sexDigitAsString);
+            return sexAsNumber % 2 == 0 ? 'F' : 'M';
+        } else {
+            return 0;
+        }
+    }
 }
