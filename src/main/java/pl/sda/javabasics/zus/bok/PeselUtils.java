b7491827b670ae8746c029bbccff92af04d36f0f
Marek Sontag <mareksontag@gmail.com>
2020-02-19 15:51:38 +0100

    Read date of birth from pesel

--- src/main/java/pl/sda/javabasics/zus/bok/PeselUtils.java
+++ src/main/java/pl/sda/javabasics/zus/bok/PeselUtils.java
@@ -1,5 +1,7 @@
 package pl.sda.javabasics.zus.bok;
 
+import java.time.LocalDate;
+
 class PeselUtils {
 
     private PeselUtils() {}
@@ -15,6 +17,31 @@ class PeselUtils {
             return sexAsNumber % 2 == 0 ? 'F' : 'M';
         } else {
             return 0;
+        }
+    }
+
+    static LocalDate readDateFromPesel(String pesel) {
+        if (!validate(pesel)) {
+            return LocalDate.MIN;
+        } else {
+            String yearAsString = pesel.substring(0, 2);
+            String codedMonthAsString = pesel.substring(2, 4);
+            String dayAsString = pesel.substring(4, 6);
+
+            int year, month;
+            int day = Integer.parseInt(dayAsString);
+
+            int codedMonth = Integer.parseInt((codedMonthAsString));
+            if (codedMonth < 20) {
+                // years: 1900 - 1999
+                year = 1900 + Integer.parseInt(yearAsString);
+                month = codedMonth;
+            } else {
+                // years: 2000 - 2099
+                year = 2000 + Integer.parseInt(yearAsString);
+                month = codedMonth - 20;
+            }
+            return LocalDate.of(year, month, day);
         }
     }
 }
